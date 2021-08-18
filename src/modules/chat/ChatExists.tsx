39e715ca6bc5f83301275587903f211252ba320d
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2021-08-18 23:13:54 +0300

    validate chat

--- src/modules/chat/ChatExists.tsx
+++ src/modules/chat/ChatExists.tsx
@@ -1,5 +1,4 @@
 import Loading from "components/loading/Loading";
-import { observer } from "mobx-react-lite";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useStore } from "stores/store";
@@ -9,22 +8,21 @@ interface ChatExistsProps {
 }
 
 const ChatExists: React.FC<ChatExistsProps> = ({ children, id }) => {
-  const { chatsRegistery } = useStore().chatStore;
+  const { validateChat } = useStore().chatStore;
   const [exists, setExists] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    if (chatsRegistery.size > 0) {
-      const exists = chatsRegistery.has(id);
+    const handleValidateChat = async () => {
+      const exists = await validateChat(id);
       setExists(exists);
 
       if (!exists) {
-        router.replace("/");
+        router.push("/");
       }
-    } else {
-      router.replace("/");
-    }
-  }, [chatsRegistery, id, exists, router]);
+    };
+    handleValidateChat();
+  }, [id, validateChat, router]);
 
   if (exists) {
     return <>{children}</>;
@@ -33,4 +31,4 @@ const ChatExists: React.FC<ChatExistsProps> = ({ children, id }) => {
   return <Loading />;
 };
 
-export default observer(ChatExists);
+export default ChatExists;
